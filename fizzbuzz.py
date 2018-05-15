5c83d9ce93f23a7edb43eaf86e1bbea60713e9ce
BrandonSersion <SersionSoftware@protonmail.com>
2018-05-15 16:25:45 -0400

    Use generator

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,26 +1,26 @@
 def fizz_buzz(fizz, buzz, limit):
-
-    # helper
+    
     def divisible_by(iterator, divisor):
         if iterator % divisor == 0:
             return True
 
     for i in range(0, limit):
         if divisible_by(i, fizz*buzz):
-            print("FizzBuzz")
-            
+            yield "FizzBuzz"
+
         elif divisible_by(i, fizz):
-            print("Fizz")
+            yield "Fizz"
 
         elif divisible_by(i, buzz):
-            print("Buzz")
+            yield "Buzz"
 
         else:
-            print(i)
+            yield str(i)
 
 
 def main():
-    fizz_buzz(3, 5, 100)
+    print('\n'.join([*fizz_buzz(3, 5, 100)]))
+    
 
 if __name__ == "__main__":
     main()
